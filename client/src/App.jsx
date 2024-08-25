0675156cf3c42ce2e251da73e03fffd56569edd4
Piyush Channawar <channawarpiyush@gmail.com>
2024-08-25 23:43:18 +0530

    Initial Boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
+import SellerDashBoard from "./pages/SellerDashBoard"
+import BuyerDashboard from "./pages/BuyerDashboard"
+import Navbar from "./components/Navbar"
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+       <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/signup" element={<Signup/>}/>
+            <Route path="/sellerdashbord" element={<SellerDashBoard/>}/>
+            <Route path="/buyerdashboard" element={<BuyerDashboard/>}/>
+          </Routes>
+       </BrowserRouter>
+    </>
+  )
+}
+
+export default App
